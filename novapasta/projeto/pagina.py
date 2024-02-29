e59e178fac78abee47cfa6a84d1e2e2366db2760
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-02-29 20:19:05 -0300

    nova_pasta

---
+++ novapasta/projeto/pagina.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def mostraAbout():
+    return render_template("about.html")
+
+@app.route("/contact")
+def mostraContact():
+    return render_template("contact.html")
+
+@app.route("/services")
+def mostraServices():
+    return render_template("services.html")

3823567fbea9d2a286f463d75e610a02721b4f58
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-03-12 21:26:35 -0300

    request

--- pasta3/projeto/pagina.py
+++ pasta3/projeto/pagina.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask (__name__)
 
@@ -9,5 +9,11 @@ def index():
 @app.route("/calc")
 def mostraCalc():
     return render_template("calc.html")
+
+@app.route("/calcular_imc", methods=["POST"])
+def calcIMC():
+    altura = request.form["txt_altura"]
+    peso = request.form["txt_peso"]
+
 
 app.run()

fd909c720f9b7f2086b98894dac5ddf96a322ec7
Hugo H. Lourenço <hugohlourenco@gmail.com>
2024-03-14 19:24:29 -0300

    verificaçao imc

--- pasta3/projeto/pagina.py
+++ pasta3/projeto/pagina.py
@@ -14,7 +14,17 @@ def mostraCalc():
 def calcIMC():
     altura = float(request.form["txt_altura"])
     peso = float(request.form["txt_peso"])
-    imc = (altura * altura) / peso
-    return render_template("calc.html", res_imc = imc)
+    imc = peso / (altura * altura)
+    if (imc < 18.5):
+        classificacao = "MAGREZA"
+    if(imc >= 18.5 and imc <= 24.9):
+        classificacao = "NORMAL"
+    if(imc >=25 and imc <= 29.9):
+        classificacao = "SOBREPESO"
+    if(imc >= 30 and imc <= 39.9):
+        classificacao = "OBESIDADE"
+    if (imc >= 40):
+        classificacao = "OBESIDADE GRAVE"
+    return render_template("calc.html", res_imc =f'{imc:.2f}', classificacao = classificacao)
 
 app.run()
